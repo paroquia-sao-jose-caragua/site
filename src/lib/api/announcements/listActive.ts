2daea2a9d882fa415212f7fb0635f6fbabe0087f
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-09-16 07:55:02 -0300

    debug: add log to trigger deploy

--- src/lib/api/announcements/listActive.ts
+++ src/lib/api/announcements/listActive.ts
@@ -2,5 +2,6 @@ import { api } from "../utils/api";
 import type { Announcement } from "@/entities/Announcement";
 
 export async function listActiveAnnouncements() {
+  console.log('listActiveAnnouncements')
   return api<{ announcements: Announcement[] }>("/announcements/active");
 }
